32f6d8696316f5855d03b65ecf8bbdd2fdc5890a
Sajithmym <sajithmym@gmail.com>
2024-04-21 11:56:18 +0530

    config

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientController } from './patient/patient.controller';
+import { PatientService } from './patient/patient.service';
+import { DoctorController } from './doctor/doctor.controller';
+import { DoctorService } from './doctor/doctor.service';
+import { PharmacyController } from './pharmacy/pharmacy.controller';
+import { PharmacyService } from './pharmacy/pharmacy.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    PatientController,
+    DoctorController,
+    PharmacyController,
+  ],
+  providers: [AppService, PatientService, DoctorService, PharmacyService],
 })
 export class AppModule {}
