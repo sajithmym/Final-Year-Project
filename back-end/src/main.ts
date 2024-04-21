5c825e68cd22e431966d0294385e4d59360aa5b7
Sajithmym <sajithmym@gmail.com>
2024-04-21 11:33:05 +0530

    eslint

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require('../Config');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(config.port);
 }
 bootstrap();
