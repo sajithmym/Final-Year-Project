0be60e9d3ed1cb60adda37751a7bae9b7c0ace07
sajithmym <sajithmym@gmail.com>
2024-04-23 10:32:25 +0530

    component

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('../Config');
@@ -7,5 +8,7 @@ const config = require('../Config');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(config.port);
+  Logger.log(`Server running on http://localhost:${config.port}`);
+  
 }
 bootstrap();
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PatientComponent } from './patient/patient.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { PharmacyComponent } from './pharmacy/pharmacy.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PatientComponent,
+    DoctorComponent,
+    PharmacyComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
