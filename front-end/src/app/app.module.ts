b9fc19808de950a576b7f53c4979240a9f60e707
Sajithmym <sajithmym@gmail.com>
2024-05-22 14:51:50 +0530

    ui

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SigninComponent } from './signin/signin.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/signin/signin.component.ts
+++ front-end/src/app/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +7,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
+  inputValue: string = ''; // Variable to store input value
+  inputValueEmail: string = ''; // Variable to store input value
+  inputValuePassword: string = ''; // Variable to store input value
+  apiData: any; // Variable to store API response
 
+  message: string = ''; // Variable to store static message
+
+  constructor(private http: HttpClient) {}
+
+  onInputChange(field: string) {
+    switch (field) {
+      case 'name':
+        console.log('Input Value (Name):', this.inputValue);
+        break;
+      case 'email':
+        console.log('Input Value (Email):', this.inputValueEmail);
+        break;
+      case 'password':
+        console.log('Input Value (Password):', this.inputValuePassword);
+        break;
+      default:
+        break;
+    }
+  }
+
+  makeApiRequest() {
+    if (this.inputValue != '' && this.inputValueEmail != '' && this.inputValuePassword != '') {
+
+      const apiUrl = 'http://localhost:3000/users'; // Example API URL
+
+      const postData = {
+        name: this.inputValue,
+        email: this.inputValueEmail,
+        password: this.inputValuePassword,
+      };
+
+      this.http.post(apiUrl, postData).subscribe(
+        (data: any) => {
+          console.log('POST Request Response:', data);
+          this.apiData = data;
+          this.message = ' - Data Inserted Successfully - '
+          alert(this.message)
+          this.disableMessage();
+          this.resetForm();
+        },
+        (error) => {
+          this.message = 'Error Occurred while Inserting Data'
+          this.disableMessage();
+        },
+      );
+    } else {
+      this.message = '-> Please Fill All The Fields <-' 
+      this.disableMessage();
+    }
+  }
+
+  resetForm() {
+    this.inputValue = '';
+    this.inputValueEmail = '';
+    this.inputValuePassword = '';
+  }
+
+  // Disable the message after 1 seconds
+  disableMessage() {
+    setTimeout(() => {
+      this.message = '';
+    }, 1000);
+  }
 }
